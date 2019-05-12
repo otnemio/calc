7359a37512b1c03eddef8b02d88f62fda2a7aa45
Mayank Sachan <otnemio@gmail.com>
2019-05-13 00:17:46 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -4,20 +4,23 @@ while True:
    print("Data Entry\t[ 2]")
    choice = input("Enter choice\t")
    if choice == '1':
+      print("Enter Expression")
       while True:
          expr = input()
          if expr == "..":
             break
          print(eval(expr))
    if choice == '2':
+      print("Enter Data")
       sum = 0
       while True:
          expr = input()
          if expr != "..":
             result = eval(expr)
-            print("\t"+str(result))
+            print("{:20.2f}".format(result))
             sum += result 
          else:
-            print("Sum:\t"+str(sum))
+            print("--------------------")
+            print("Total:"+"{:14.2f}".format(sum))
+            print("--------------------")
             break
-         
